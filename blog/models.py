cfe137c960e88741c3ae3610e8f04c7922197afd
socheathorl <socheathorl173@gmail.com>
2022-12-13 09:18:38 +0700

    integrate ckeditor

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+class PublishedManager(models.Manager):
+  def get_queryset(self):
+    return super(PublishedManager,self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -14,7 +21,7 @@ class Post(models.Model):
   title = models.CharField(max_length=250)
   slug = models.SlugField(max_length=250, unique_for_date='publish')
   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-  body = models.TextField()
+  body = RichTextUploadingField()
   publish = models.DateTimeField(default=timezone.now)
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
@@ -25,3 +32,10 @@ class Post(models.Model):
 
   def __str__(self):
     return self.title
+
+  objects = models.Manager() # The default manager.
+  published = PublishedManager() # Our custom manager.
+
+  def get_absolute_url(self):
+    return reverse('blog:post_detail',args=[self.slug])
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+  posts = Post.published.all()
+
+  paginator = Paginator(posts, 2) # 10 posts in each page
+  page = request.GET.get('page')
+  try:
+    posts = paginator.page(page)
+  except PageNotAnInteger:
+    # If page is not an integer deliver the first page
+    posts = paginator.page(1)
+  except EmptyPage:
+    # If page is out of range deliver last page of results
+    posts = paginator.page(paginator.num_pages)
+
+  return render(request,'post_list.html',{'posts':posts})
+
+
+def post_detail(request, post):
+  post=get_object_or_404(Post,slug=post,status='published')
+  return render(request, 'post_detail.html',{'post':post})
