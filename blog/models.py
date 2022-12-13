b6bf04b19f5285f56c344ff2adecfb76abd0f3ca
socheathorl <socheathorl173@gmail.com>
2022-12-13 09:35:00 +0700

    image feature

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,7 @@ class Post(models.Model):
 
   title = models.CharField(max_length=250)
   slug = models.SlugField(max_length=250, unique_for_date='publish')
+  image = models.ImageField(upload_to='featured_image/%Y/%m/%d/')
   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
   body = RichTextUploadingField()
   publish = models.DateTimeField(default=timezone.now)
